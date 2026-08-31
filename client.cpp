ac65bdcb9b75b5f2675499c055d978a3c5d4a42a
Shih-Hung Sun <fatharley2001@gmail.com>
2026-08-31 17:20:46 +0900

    Implement barebones client/server app

--- client.cpp
+++ client.cpp
@@ -1,1 +1,82 @@
 
+#include <iostream>
+#include <cstring>
+#include <array>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+
+/**
+ * Client workflow:
+ *
+ *  1. 建立一個 TCP socket
+ *  2. 建立 server 的 IPv4 address information
+ *  3. 填入 AF_INET
+ *  4. 填入 server port
+ *  5. 填入 127.0.0.1
+ *  6. 呼叫 connect()
+ *  7. 準備一段要傳送的文字
+ *  8. 呼叫 send()，或重用你已經理解的 send_all()
+ *  9. 準備接收 buffer
+ * 10. 呼叫 recv()
+ * 11. 根據 recv() 回傳的 byte 數處理資料
+ * 12. close()
+ */
+
+
+constexpr int SERVER_PORT = 8080;
+
+void die(std::string operation) {
+    std::cerr << operation << ": " 
+        << std::strerror(errno) << std::endl; 
+    std::exit(EXIT_FAILURE);
+}
+
+int main() {
+    std::cout << "creating client socket" << std::endl;
+
+    // create a TCP socket
+    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_fd == -1) {
+        die("socket");
+    }
+    
+    sockaddr_in server_addr{};  // the zero initializer '{}' is needed, it zeroes everything out (like memset in C)
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+    // connect to server
+    if (connect(client_fd, reinterpret_cast<sockaddr*>(&server_addr), sizeof(server_addr)) == -1) {
+        die("connect");
+    }
+
+    // for prototying, ask for user input, send server the input
+    // and server will output the same thing
+
+    std::cout << "Give me something: ";
+    std::string in; 
+    getline(std::cin, in);  // get user input
+
+    std::cout << "Sending " << in << "..." << std::endl;
+    if (send(client_fd, in.data(), in.size(), 0) != in.size()) {
+        die("send");
+    }
+
+    std::string received(in.size(), ' ');
+    const ssize_t bytes_received = recv(client_fd, received.data(), received.size(), 0);
+    if (bytes_received != in.size()) {
+        close(client_fd);
+        die("recv");
+    } else {
+        // print out what's been received
+        std::cout << received << std::endl;
+    }
+    close(client_fd);
+}
+
+
+
+
--- server.cpp
+++ server.cpp
@@ -6,6 +6,14 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
+
+/**  
+ * The basic routine is: server will wait for a connection,
+ * accept() it, and fork() a child process to handle it.
+ * 
+ * source: https://beej.us/guide/bgnet/html/split/client-server-background.html
+ */
+
 constexpr int SERVER_PORT = 8080;
 
 void die(std::string operation) {
@@ -16,16 +24,16 @@ void die(std::string operation) {
 
 int main() {
     // classic socket-bind-listen-accept pattern.
-    std::cout << "creating server socket" << std::endl;
+    std::cout << "Creating server socket" << std::endl;
 
     // create a TCP socket
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) {
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
         die("socket");
     }
     
     int on = 1;
-    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) == -1) {
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) == -1) {
         die("setsocketopt");
     }
     sockaddr_in server_addr{};  // the zero initializer '{}' is needed, it zeroes everything out (like memset in C)
@@ -34,20 +42,20 @@ int main() {
     server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
     // bind the socket
-    if (bind(server_socket, reinterpret_cast<sockaddr *>(&server_addr), sizeof(server_addr)) == -1) {
+    if (bind(server_fd, reinterpret_cast<sockaddr *>(&server_addr), sizeof(server_addr)) == -1) {
         die("bind");
     }
 
     // starts listening
-    if (listen(server_socket, 10) == -1) {
+    if (listen(server_fd, 10) == -1) {
         die("listen");
     }
     std::cout << "Server listening on 127.0.0.1:" << SERVER_PORT << '\n';
 
     // accepts connection
     while (true) {
-        const int conn = accept(server_socket, nullptr, nullptr);
-        if (conn == -1) {
+        const int conn_fd = accept(server_fd, nullptr, nullptr);
+        if (conn_fd == -1) {
             // EINTR is interrupt, resume if err due to interruption
             if (errno == EINTR) {
                 continue;
@@ -55,14 +63,15 @@ int main() {
             die("accept");
         }
         std::array<char, 1024> buf{};
-        const ssize_t bytes_received = recv(conn, buf.data(), buf.size(), 0);
+        const ssize_t bytes_received = recv(conn_fd, buf.data(), buf.size(), 0);
         if (bytes_received == -1) {
-            close(conn);
+            close(conn_fd);
             die("recv");
         } else if (bytes_received > 0) {
-            send(conn, buf.data(), bytes_received, 0);
+            std::cout << "Received " << std::string(buf.data(), bytes_received) << std::endl;
+            send(conn_fd, buf.data(), bytes_received, 0);
         }
-        close(conn);
+        close(conn_fd);
     }
 }
 
